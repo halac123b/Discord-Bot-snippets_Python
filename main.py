f3fa0816c12647f4a172ee521e9e589c9f5fb9dd
halac123b <halac123b@gmail.com>
2024-02-02 21:27:22 +0700

    2/2/24

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import os
 client = discord.Client()
 
 
+# On Bot Ready
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+
+
+# On Message
+@client.event
+async def on_message(message):
+    # Nếu là msg từ chính bot thì bỏ qua
+    if message.author == client.user:
+        return
+    # Pattern các câu lệnh
+    if message.content.startswith("$hello"):
+        # Gửi msg trả lời
+        await message.channel.send("Hello!")
+
+
+client.run(os.getenv("TOKEN"))
