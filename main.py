bab7c0f7d45034c5a2cd889acb32d47c36a74720
haoptimizervn <nguyen_duy_ha@optimizer.co.jp>
2024-02-04 11:41:27 +0700

    4/2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import os
 import dotenv
+import requests
+import json
+import random
 
 dotenv.load_dotenv()
 
@@ -9,6 +12,22 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+starter_encouragements = [
+    "Cheer up!",
+    "Hang in there.",
+    "You are a great person / bot!",
+]
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    # Parse response thành json
+    json_data = json.loads(response.text)
+    # Lấy quote và author từ json
+    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+    return quote
 
 
 # On Bot Ready
@@ -23,10 +42,20 @@ async def on_message(message):
     # Nếu là msg từ chính bot thì bỏ qua
     if message.author == client.user:
         return
+
+    msg = message.content
+
     # Pattern các câu lệnh
-    if message.content.startswith("$hello"):
+    if msg.startswith("$hello"):
         # Gửi msg trả lời
         await message.channel.send("Hello!")
+
+    if msg.startswith("$inspire"):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
 
 
 client.run(os.getenv("TOKEN"))
